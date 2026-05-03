3d3140b51c2e07840b30e0060a62c6801a513cd8
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2026-05-03 19:00:04 +0600

    cors setup

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,11 +6,15 @@ const uri = process.env.DB_URL
 const client = new MongoClient(uri)
 const db = client.db("skillsphere_auth_db")
 export const auth = betterAuth({
+    trustedOrigins: ["https://skillsphere-eosin.vercel.app"],
     socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }
+    },
+    advanced: {
+        crossOrigin: true
     },
     emailAndPassword: {
         enabled: true
